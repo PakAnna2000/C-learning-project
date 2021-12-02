28f38e01e34ce16ac9b1d3dc0f1111e449c9074e
PakAnna2000 <pakannaolegovna@gmail.com>
2021-12-02 17:20:29 +0500

    1 program Saturnalia with +---+ borders completed

--- C_learn.cs
+++ C_learn.cs
@@ -1,17 +1,69 @@
 ﻿using System;
 
-namespace MyApp // Note: actual namespace depends on the project name.
+namespace C_learn // Note: actual namespace depends on the project name.
 {
-
     public class Program
     {
-        public static void menuLine(int i, string str) 
+        public static void menuLine(int i, string str) //function to make line in
+        //"i. str: " format
         {
             Console.WriteLine(i + ". " + str + ": ");
         }
+        public static string? stringInput(string str) // for string input
+        {
+            Console.Write(str + ": ");      
+            string? input = Console.ReadLine();  
+            return input;
+        }
+        public static int intInput(string str) // for int input
+        {
+            Console.Write(str + ": ");      
+            return int.Parse(Console.ReadLine()); // converting string to int
+        }
+        public static void drawLine(int size) // draw +----+
+        {
+            Console.Write("+");
+            for (int i = 0; i < size + 2; i++)
+            {
+                Console.Write("-");
+            }
+            Console.Write("+\n");
+        }
+        public static void decorate(string str) // make borders
+        {
+            int size = str.Length;
+            drawLine(size);
+            Console.WriteLine("| " + str + " |");
+            drawLine(size);
+        }
+        public static void saturnalia() // 1. Saturnalia
+        {
+            int lineNum = intInput("Lines");
+            string[] lines = new string[lineNum];
+            for(int i = 0; i < lineNum; i++) 
+            {
+                lines[i] = stringInput((i+1).ToString());
+            }
+            for(int i = 0; i < lineNum; i++)
+            {
+                Console.WriteLine("Case #" + (i + 1));
+                decorate(lines[i]);
+            }
+        }
         public static void Main(string[] args)
         {
-            
+            int progNumber = 1;
+            switch(progNumber)
+            {
+                case 1: 
+                {
+                    //Saturnalia
+                    menuLine(progNumber, "Saturnalia");
+                    saturnalia();
+                    break;
+                }
+                default: break;
+            }
         }
     }
 }
