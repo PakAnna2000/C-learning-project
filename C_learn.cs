47a0f48463b67e4f19a30ba8f7bbabde08327db6
PakAnna2000 <pakannaolegovna@gmail.com>
2021-12-02 16:00:46 +0500

    menuLine() function added, extended start template used

---
+++ C_learn.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace MyApp // Note: actual namespace depends on the project name.
+{
+
+    public class Program
+    {
+        public static void menuLine(int i, string str) 
+        {
+            Console.WriteLine(i + ". " + str + ": ");
+        }
+        public static void Main(string[] args)
+        {
+            
+        }
+    }
+}
